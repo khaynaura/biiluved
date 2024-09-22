a67494c27eb17ec8964ba182f90a866f1a4e6127
khaynaura <146971893+khaynaura@users.noreply.github.com>
2024-09-23 00:09:03 +0700

    finished tugas 4

--- biiluved/settings.py
+++ biiluved/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(5_ajzk)^k)4_x!w5lt2j%$&)+za(21o5hu%^x(6wkx5@oaesi"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "khayla-naura-biiluved.pbp.cs.ui.ac.id"]
 
